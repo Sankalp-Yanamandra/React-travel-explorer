580b22090e2b5ab7cc541880aa8d3d9de8f56752
shivan-987 <174676664+shivan-987@users.noreply.github.com>
2026-06-13 20:20:50 +0530

    project setup, react router, JSON router, axios, resuable components, dynamic routing, destination details page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Home() {
+ return (
+   <div>
+     <h1>Travel Explorer</h1>
+
+     <p>
+       Discover amazing destinations around
+       the world.
+     </p>
+   </div>
+ );
+}
+
+export default Home;
---
+++ src/routes/Approutes.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import { Routes, Route } from "react-router-dom";
+
+import Home from "../pages/Home";
+import Destinations from "../pages/Destinations";
+import DestinationDetails from "../pages/DestinationDetails";
+
+function AppRoutes() {
+ return (
+   <Routes>
+     <Route
+       path="/"
+       element={<Home />}
+     />
+
+     <Route
+       path="/destinations"
+       element={<Destinations />}
+     />
+
+     <Route
+       path="/destinations/:id"
+       element={<DestinationDetails />}
+     />
+   </Routes>
+ );
+}
+
+export default AppRoutes;
